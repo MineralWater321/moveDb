5071953b55d684d5dfa22c0496bf1a657da3ac3c
Edward Paler <paler.edward@gmail.com>
2022-07-03 23:48:12 +0800

    Working site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			'https://api.themoviedb.org/3/movie/popular?api_key=be2632d704a7156167f22c5b5f47a62e&language=en-US&page=1'
+		);
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	import SearchMovies from '../components/SearchMovies.svelte';
+	export let popular;
+	import { fly } from 'svelte/transition';
+</script>
+
+<section in:fly={{ y: 50, duration: 500, delay: 500 }} out:fly={{ duration: 500 }}>
+	<SearchMovies />
+	<PopularMovies {popular} />
+</section>
